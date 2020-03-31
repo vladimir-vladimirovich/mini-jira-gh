32ac68b281af3adb8cf508d0943fe797d5be0ca0
Vladimir <vladimir.ovcharov.v@gmail.com>
2020-03-31 16:06:51 +0300

    Updates

---
+++ precache-manifest.ebf16c8a382620c8e8667239ad5b2071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5b7b24b2c37c683dad1c697257a8b1",
+    "url": "/mini-jira-gh/index.html"
+  },
+  {
+    "revision": "a6dc723517fd1a8cef28",
+    "url": "/mini-jira-gh/static/css/main.d798762b.chunk.css"
+  },
+  {
+    "revision": "493b07a47913e549b851",
+    "url": "/mini-jira-gh/static/js/2.2cc2e125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-jira-gh/static/js/2.2cc2e125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6dc723517fd1a8cef28",
+    "url": "/mini-jira-gh/static/js/main.e603934b.chunk.js"
+  },
+  {
+    "revision": "6a48aa4d110d44181c52",
+    "url": "/mini-jira-gh/static/js/runtime-main.0c95ebe7.js"
+  }
+]);
